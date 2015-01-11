fed84349daf89fd596a3b18cc2fc4489012616c2
antoineludwig <antoine.ludwig60@gmail.com>
2015-01-11 15:51:43 +0100

    Premier commit

---
+++ app/src/main/java/com/projet/esgi/meteoesgial1/ListeVilles.java
@@ -0,0 +1,52 @@
+package com.projet.esgi.meteoesgial1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+
+public class ListeVilles extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_villes);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_liste_villes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //récupère la liste de ville
+        ListView li = (ListView) findViewById(R.id.listeVille);
+        li.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TO DO ouvre l'autre activity
+            }
+        });
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
